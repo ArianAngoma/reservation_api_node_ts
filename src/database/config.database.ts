32b41d67ba9a30b01b16b9b7c258aabefe9f3034
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-06-11 21:10:30 -0500

    Test configuration

--- src/database/config.database.ts
+++ src/database/config.database.ts
@@ -1,16 +1,24 @@
 import mongoose from 'mongoose';
+import {MongoMemoryServer} from 'mongodb-memory-server';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {DB_MONGO} = process.env;
+const {DB_MONGO, DB_MONGO_TEST, NODE_ENV} = process.env;
 
-const connectionString = DB_MONGO;
+const connectionString = NODE_ENV === 'test' ? DB_MONGO_TEST : DB_MONGO;
 
 export const dbConnection = async (): Promise<void> => {
   try {
-    await mongoose.connect(connectionString!);
-    console.log('DB online');
+    if (connectionString === 'inmemory') {
+      const mongoServer: MongoMemoryServer = await MongoMemoryServer.create();
+      const mongoUrl = mongoServer.getUri();
+      await mongoose.connect(mongoUrl);
+    } else {
+      await mongoose.connect(connectionString!);
+      console.log('DB online');
+    }
   } catch (error) {
     console.log(error);
     throw new Error(`Error connecting to DB`);
